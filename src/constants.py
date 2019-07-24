898c9f6d145ecbd89398f4d978bb362d6bf40e57
Deepak Seeni <dseeni@gmail.com>
2019-07-24 13:41:04 -0700

    Update constants.py with files, prasers, named tuple names, and playground.py

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,23 @@
 from src.parse_utils import parse_date
 
 
-print('4:', 'parse_date() ''='' ', parse_date())
+# Files
+fpersonal = 'data/personal_info.csv'
+femployment = 'data/employment.csv'
+fvehicles = 'data/vehicles.csv'
+fupdate_status = 'data/update_status.csv'
+fnames = fpersonal, femployment, fvehicles, fupdate_status
 
+# Parsers
+personal_parser = (str, str, str, str, str)
+employment_parser = (str, str, str, str)
+vehicle_parser = (str, str, str, int)
+update_status_parser = (str, parse_date, parse_date)
+parsers = personal_parser, employment_parser, vehicle_parser, update_status_parser
+
+# Named Tuple Names
+personal_class_name = 'Personal'
+employment_class_name = 'Employment'
+vehicle_class_name = 'Vehicle'
+update_status_class_name = 'UpdateStatus'
+class_names = personal_class_name, employment_class_name, vehicle_class_name, update_status_class_name
