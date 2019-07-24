1e73bcf7a63297daac7120f545260bff0b64f066
Deepak Seeni <dseeni@gmail.com>
2019-07-24 14:24:53 -0700

    Fix to import and read lines no more cyclical import error

--- src/parse_utils.py
+++ src/parse_utils.py
@@ -1,8 +1,6 @@
 from itertools import islice
-from src.constants import fnames, parsers, class_names
 from csv import reader
 from datetime import date
-from src.constants import *
 
 # read file as csv
 def read_file(file_name):
@@ -10,9 +8,18 @@ def read_file(file_name):
         rows = reader(f, delimiter=',', quotechar='"')
         yield from rows
 
-def header_extract(file_names):
+
+# read N lines for file in fnames
+def read_file_rows(file_names, lines: int):
     for file in file_names:
-        with open(file) as f:
+        rows = read_file(file)
+        for row in islice(rows, lines):
+            print(row)
+
+
+def header_extract(file_names):
+    for file_name in file_names:
+        with open(file_name) as f:
             header = reader(f, delimiter=',', quotechar='"')
             return header
     # with open(self.filename) as file:
@@ -21,11 +28,7 @@ def header_extract(file_names):
     #     self.data_key = data_string.split(',')
     #     self.infer_data_type()
 
-# read N lines for file in fnames
-for file in src.constants.fnames:
-    lines = read_file(file)
-    for line in islice(lines, 5):
-        print(line)
+
 
 
 def cast(single_data_value, data_value):
@@ -40,38 +43,38 @@ def cast(single_data_value, data_value):
             return None
         return str(data_value)
 
-def date_modifier(date_string: str) -> date:
-    date_list = date_string.split('/')
-    # noinspection PyUnusedLocal
-    date_format_key = ['int' for i in range(3)]
-    print('121:', 'date_format_key ''='' ', date_format_key)
-    finaldate = [FileReader.cast(date_format_key, date_list)
-                 for date_format_key, date_list in zip(date_format_key, date_list)]
-    assert all(isinstance(i, int) for i in finaldate)
-    date_object = date(finaldate[2], finaldate[0], finaldate[1])
-    # assert date_object.year == 2016
-    # assert date_object.month == 5
-    # assert date_object.day == 10
-    return date_object
+# def date_modifier(date_string: str) -> date:
+#     date_list = date_string.split('/')
+#     # noinspection PyUnusedLocal
+#     date_format_key = ['int' for i in range(3)]
+#     print('121:', 'date_format_key ''='' ', date_format_key)
+#     finaldate = [FileReader.cast(date_format_key, date_list)
+#                  for date_format_key, date_list in zip(date_format_key, date_list)]
+#     assert all(isinstance(i, int) for i in finaldate)
+#     date_object = date(finaldate[2], finaldate[0], finaldate[1])
+#     # assert date_object.year == 2016
+#     # assert date_object.month == 5
+#     # assert date_object.day == 10
+#     return date_object
 
 
 
 
-def infer_data_type(self):
-    for value in self.data_key:
-        if value is None:
-            self.data_key[self.data_key.index(value)] = None
-        elif all(c.isdigit() for c in value):
-            self.data_key[self.data_key.index(value)] = int(value)
-
-        elif value.count('.') == 1:
-            try:
-                self.data_key[self.data_key.index(value)] = float(value)
-            except ValueError:
-                self.data_key[self.data_key.index(value)] = str(value)
-
-        else:
-            self.data_key[self.data_key.index(value)] = str(value)
+# def infer_data_type(self):
+#     for value in self.data_key:
+#         if value is None:
+#             self.data_key[self.data_key.index(value)] = None
+#         elif all(c.isdigit() for c in value):
+#             self.data_key[self.data_key.index(value)] = int(value)
+#
+#         elif value.count('.') == 1:
+#             try:
+#                 self.data_key[self.data_key.index(value)] = float(value)
+#             except ValueError:
+#                 self.data_key[self.data_key.index(value)] = str(value)
+#
+#         else:
+#             self.data_key[self.data_key.index(value)] = str(value)
 
 def parse_date():
     return 'temp_date'
--- src/playground.py
+++ src/playground.py
@@ -1,1 +1,4 @@
+from src.parse_utils import *
+from src.constants import *
 
+print(read_file_rows(fnames, 5))
