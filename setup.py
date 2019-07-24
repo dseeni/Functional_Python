2a8594d86e6091ed2a243612f4968a97c7672680
Deepak Seeni <dseeni@gmail.com>
2019-07-24 11:08:14 -0700

    First commit add base files for PDD2_Project4

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PDD2_Project_4',
+    version='v0.001',
+    description='Python Deep Dive Part 2 Project 4',
+    author='Deepak Seeni',
+    author_email='dseeni@gmail.com',
+    url='https://github.com/dseeni/PDD2_Project4',
+    packages=find_packages()
+)
