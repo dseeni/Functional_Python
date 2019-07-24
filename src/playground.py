5f3039d033dfef91323cfd7ff9d4608b060e80d2
Deepak Seeni <dseeni@gmail.com>
2019-07-24 14:07:28 -0700

    WIP fix to import errors

--- src/playground.py
+++ src/playground.py
@@ -1,18 +1,1 @@
-from itertools import islice
-from src.constants import fnames, parsers, class_names
-from csv import reader
-
-
-# read file as csv
-def read_file(file_name):
-    with open(file_name) as f:
-        rows = reader(f, delimiter=',', quotechar='"')
-        yield from rows
-
-
-# read N lines for file in fnames
-for file in fnames:
-    lines = read_file(file)
-    for line in islice(lines, 5):
-        print(line)
 
