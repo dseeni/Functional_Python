1690bcef6bb2c9e1f30d63d747742673cf0a917c
Deepak Seeni <dseeni@gmail.com>
2019-07-24 23:50:51 -0700

    Add final fucntions for Goal_1, almost done..

--- src/parse_utils.py
+++ src/parse_utils.py
@@ -1,19 +1,20 @@
 from itertools import islice
 from csv import reader
 from datetime import datetime
+from collections import namedtuple
 
 
 # read file as csv
-def read_csv_file(file_name):
+def csv_reader(file_name):
     with open(file_name) as f:
         rows = reader(f, delimiter=',', quotechar='"')
-        yield rows
+        yield from rows
 
 
 # read N lines for file in fnames
 def row_csv_extract(file_names, lines: int):
     for file in file_names:
-        rows = read_csv_file(file)
+        rows = csv_reader(file)
         for row in islice(rows, lines):
             print(row)
 
@@ -22,7 +23,7 @@ def row_csv_extract(file_names, lines: int):
 def header_extract(file_name):
     with open(file_name) as f:
         file_line = reader(f, delimiter=',', quotechar='"')
-        yield next(file_line)
+        return next(file_line)
 
 
 def data_row_extract(file_name):
@@ -31,6 +32,21 @@ def data_row_extract(file_name):
         # skip the header line
         next(file_line)
         yield next(file_line)
+
+
+# class_name = name of the file personal_info, employment, etc
+def create_named_tuple_class(fname, class_name):
+    fields = header_extract(fname)
+    return namedtuple(class_name, fields)
+
+
+def iter_file(fname, class_name, parser):
+    nt_class = create_named_tuple_class(fname, class_name)
+    reader = csv_reader(fname)
+    for row in reader:
+        parsed_data = (cast(element, data_type) for element, data_type in zip(row, parser))
+        yield nt_class(*parsed_data)
+
 
 
 def cast(element, data_type):
--- src/playground.py
+++ src/playground.py
@@ -1,26 +1,8 @@
 from src.parse_utils import *
 from src.constants import *
-import os
-from itertools import chain
-from collections import namedtuple
 
-headers = list(header_extract(fnames))
-print(headers)
-# print(len(headers))
-# print(list(data_row_extract(fnames)))
-final_rows_data_types = [list(zip(list(data_row_extract(fnames))[i], parsers[i])) for i in range(len(parsers))]
-print(final_rows_data_types)
-
-data = [cast(i[0], i[1]) for i in final_rows_data_types for j in i]
-# print(final_rows_data_types)
-# print(data)
-
-# namedtuples =  [namedtuple('final_data_row', header) for header in headers]
-# for i in range(len(final_rows)):
-#     finalnamed = namedtuples[i]
-#     finalrownamed = finalnamed(*final_rows[i])
-#     print(*finalrownamed, sep='\n')
-
-
+row = iter_file(fnames[0], 'personal_info', parsers[0])
+for rows in csv_reader(fnames[0]):
+    print(next(row))
 
 
