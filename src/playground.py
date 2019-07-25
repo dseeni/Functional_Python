1133b1f45286b6eef67392af8243ea4532a741bb
Deepak Seeni <dseeni@gmail.com>
2019-07-24 17:51:44 -0700

    Add minor tweaks

--- src/playground.py
+++ src/playground.py
@@ -2,8 +2,7 @@ from src.parse_utils import *
 from src.constants import *
 
 # print(read_file_rows(fnames, 5))
-
-# fnames = fpersonal, femployment, fvehicles, fupdate_status
+fnames = fpersonal, femployment, fvehicles, fupdate_status
 print(*list(header_extract(fnames)), sep='\n')
 
 
