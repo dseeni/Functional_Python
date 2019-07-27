26227c2ce2c09de691f5ee985cab78c88f0683f6
Deepak Seeni <dseeni@gmail.com>
2019-07-27 08:45:27 -0700

    Add 4 file for loop in prase_utils.py

--- src/playground.py
+++ src/playground.py
@@ -10,6 +10,7 @@ from itertools import starmap
 # rows_csv_extract(fnames, 3)
 Personal_Info = create_named_tuple_class('Personal_Info', fnames[0])
 row = data_row_extract(fnames[0])
+zipped_row = zip_type_key(next(row), parsers[0])
 # print(next(row))
 # print(zip_type_key(next(row), parsers[0]))
 # print(zip_type_key(next(row), parsers[0]))
@@ -17,12 +18,16 @@ row = data_row_extract(fnames[0])
 # print(zip_type_key(row, parsers[0]))
 # print(tuple(header_extract(fnames[0])))
 
-zipped_row = zip_type_key(next(row), parsers[0])
-print(zipped_row)
-print(cast_zipped_row(zipped_row))
+# print(zipped_row)
+# print(cast_zipped_row(zipped_row))
 
-print(list(iter_file(fnames[0], 'Personal_Info', parsers[0])))
-print(len(list(iter_file(fnames[0], 'Personal_Info', parsers[0]))))
+# print(list(iter_file(fnames[0], 'Personal_Info', parsers[0])))
+# print(len(list(iter_file(fnames[0], 'Personal_Info', parsers[0]))))
 # assert list(parsers[0]) == list(type(i) for i in cast_zipped_row(zipped_row))
 
-
+for filename, class_name, parser in zip(fnames, class_names, parsers):
+    file_iter = iter_file(filename, class_name, parser)
+    print(filename)
+    for _ in range(10):
+        print(next(file_iter))
+    print()
